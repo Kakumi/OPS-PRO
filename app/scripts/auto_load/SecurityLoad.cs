b15ba52f82f96b58be1900cb16c47eaa46d82276
Damien Brebion <mangasa2000@hotmail.com>
2023-03-21 23:58:10 +0100

    Allow debug build to bypass security load

--- app/scripts/auto_load/SecurityLoad.cs
+++ app/scripts/auto_load/SecurityLoad.cs
@@ -6,10 +6,13 @@ public partial class SecurityLoad : Node
 {
     public override void _Ready()
     {
-        var args = OS.GetCmdlineArgs().ToList<string>();
-        if (!args.Any(x => x == "Wg9*8Z49UYvU&yU@@F"))
+        if (!OS.IsDebugBuild())
         {
-            GetTree().Quit();
+            var args = OS.GetCmdlineArgs().ToList<string>();
+            if (!args.Any(x => x == "Wg9*8Z49UYvU&yU@@F"))
+            {
+                GetTree().Quit();
+            }
         }
     }
 }
